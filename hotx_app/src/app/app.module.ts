ab5ca2e3b1216a0647ea20ed19601566cc044791
jitendrapatel <jpatel@ximplesolution.com>
2023-03-05 19:12:50 -0800

    Second commit(Navbar and Form

--- hotx_app/src/app/app.module.ts
+++ hotx_app/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavbarComponent } from 'src/navbar/navbar.component';
+import { FormComponent } from 'src/form/form.component';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule
