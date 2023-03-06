f73624974390659d6b5401079a047746d9a7d4c1
jitendrapatel <jpatel@ximplesolution.com>
2023-03-05 20:32:43 -0800

    Working Code

--- hotx_app/src/app/app.module.ts
+++ hotx_app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from 'src/navbar/navbar.component';
 import { FormComponent } from 'src/form/form.component';
 import { AppComponent } from './app.component';
+import { SampleComponent } from 'src/sample/sample.component';
 @NgModule({
   declarations: [
 
     AppComponent,
     NavbarComponent,
     FormComponent,
+    SampleComponent,
   ],
   imports: [
     BrowserModule
